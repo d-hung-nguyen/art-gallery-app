72d6f1098869b48d420f5fb017cf6378d663c518
Duy Hung Nguyen <nd.hung@me.com>
2024-02-16 16:36:15 +0100

    nav
    nav

---
+++ pages/art-pieces/index.js
@@ -0,0 +1,10 @@
+import Nav from "@/components/Nav";
+
+export default function ArtPieces() {
+  return (
+    <div>
+      <h1>Hello from Next.js</h1>
+      <Nav/>
+    </div>
+  );
+}
---
+++ pages/favorites.js
@@ -0,0 +1,12 @@
+
+
+import Nav from "@/components/Nav";
+
+export default function Favorites() {
+  return (
+    <div>
+      <h1>Hello from Next.js</h1>
+      <Nav/>
+    </div>
+  );
+}
